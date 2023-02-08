0001efbf963e29aabae94262dc36df6efe2f93f9
LMiti3030 <mititelu.laura@yahoo.com>
2023-02-08 16:34:59 +0200

    cleanup comments

--- src/main/java/laura/mititelu/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
+++ src/main/java/laura/mititelu/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-//    @RequestMapping("/hello-world")
-    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
