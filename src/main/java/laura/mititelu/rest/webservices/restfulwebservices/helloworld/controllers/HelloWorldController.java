fc0298a455af5014340af484e69bf71efb40b48a
LMiti3030 <mititelu.laura@yahoo.com>
2023-02-08 16:50:53 +0200

    Enhancing the Hello World REST API to Return a Bean

--- src/main/java/laura/mititelu/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
+++ src/main/java/laura/mititelu/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package laura.mititelu.rest.webservices.restfulwebservices.helloworld.controllers;
 
+import laura.mititelu.rest.webservices.restfulwebservices.helloworld.models.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 
 }
